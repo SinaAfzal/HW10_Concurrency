6c0ee24798d2e6299ebef963806e0dd28558d0ca
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-10-18 18:53:00 +0330

    create Q1Application class and solve question1

---
+++ src/q1/Q1Application.java
@@ -0,0 +1,74 @@
+package q1;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q1Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int input=0;
+        boolean isValidInteger = false;
+        while (!isValidInteger) {/// getting a valid integer from user
+            System.out.println("Enter an Integer: ");
+            try {
+                input = scanner.nextInt();
+                scanner.nextLine();
+                isValidInteger = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! please enter and Integer: ");
+                scanner.nextLine();
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        int finalInput = input;//making input effectively final so it can be used in lambda expression.
+        Thread evenInserterThread = new Thread(() -> {
+            for (int i = 0; i <= finalInput; i += 2) {
+                if (i != finalInput) {// this is to prevent from a deadlock to occur at i=finalInput. without this condition, the program will wait forever at i=finalinput if the input is an even number.
+                    synchronized (list) {
+                        list.add(i);
+                        System.out.println("evenInserterThread added " + i);
+                        try {
+                            list.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        list.notifyAll();
+                    }
+                } else {
+                    System.out.println("evenInserterThread added " + i);
+                    list.add(i);
+                }
+            }
+
+        });
+
+        Thread oddInserterThread = new Thread(() -> {
+            for (int i = 1; i <= finalInput; i += 2) {
+                synchronized (list) {
+                    list.add(i);
+                    System.out.println("oddInserterThread added " + i);
+                    list.notifyAll();
+                    try {
+                        list.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        evenInserterThread.start();
+        oddInserterThread.start();
+        try {
+            evenInserterThread.join();
+            oddInserterThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("List: " + list);
+    }
+}
